a3679ea5b4d4d8d43f1afbf04ae5b6b89a83b12c
Mishar025 <mishamay583@gmail.com>
2025-04-11 17:09:16 -0400

    Added new files

--- face-check-ui/src/app/modules/main-page/main-page.module.ts
+++ face-check-ui/src/app/modules/main-page/main-page.module.ts
@@ -3,15 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { MainPageRoutingModule } from './main-page-routing.module';
 import {HomeComponent} from "./pages/home/home.component";
-import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignInComponent } from './authentication/sign-in/sign-in.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { PricingComponent } from './pages/pricing/pricing.component';
 import { AdminPageComponent } from './pages/admin-page/admin-page.component';
 import { UserPageComponent } from './pages/user-page/user-page.component';
-import { VerificationCodeComponent } from './pages/verification-code/verification-code.component';
+import { VerificationCodeComponent } from './authentication/verification-code/verification-code.component';
 import {CodeInputModule} from "angular-code-input";
 import { ReactiveFormsModule } from '@angular/forms';
-import {RegisterComponent} from "./pages/register/register.component";
+import {RegisterComponent} from "./authentication/register/register.component";
+import { RegisterCompanyComponent } from './authentication/register-company/register-company.component';
 
 
 @NgModule({
@@ -23,7 +24,8 @@ import {RegisterComponent} from "./pages/register/register.component";
       RegisterComponent,
       AdminPageComponent,
       UserPageComponent,
-      VerificationCodeComponent
+      VerificationCodeComponent,
+      RegisterCompanyComponent
   ],
   imports: [
     CommonModule,
