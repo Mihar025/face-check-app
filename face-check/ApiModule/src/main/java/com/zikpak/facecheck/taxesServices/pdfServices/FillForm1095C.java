d020f41d63d0acf895a796181e9e13015f03fd00
Mishar025 <mishamay583@gmail.com>
2025-05-29 22:46:05 -0400

    ,

---
+++ face-check/ApiModule/src/main/java/com/zikpak/facecheck/taxesServices/pdfServices/FillForm1095C.java
@@ -0,0 +1,9 @@
+package com.zikpak.facecheck.taxesServices.pdfServices;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class FillForm1095C {
+}
