1d7b15f1af8648260a1f214dd9d39fc16370ab78
Mishar025 <mishamay583@gmail.com>
2025-07-18 18:31:29 -0400

    ,

--- face-check/SecurityDomainModule/src/main/java/com/zikpak/facecheck/domain/AdminAndForemanFunctionality.java
+++ face-check/SecurityDomainModule/src/main/java/com/zikpak/facecheck/domain/AdminAndForemanFunctionality.java
@@ -5,8 +5,6 @@ import com.zikpak.facecheck.requestsResponses.admin.WorksiteWorkerResponse;
 import org.springframework.security.core.Authentication;
 
 public interface AdminAndForemanFunctionality {
-
-
     PageResponse<WorksiteWorkerResponse> findAllWorkersInWorkSite(int page, int size, Integer workSiteId, Authentication authentication);
 
 
