9ad8aa5076f3aa128b5209b15bd2cacaf0c9de0a
Mishar025 <mishamay583@gmail.com>
2025-07-20 21:27:28 -0400

    ,

--- face-check/SecurityDomainModule/src/main/java/com/zikpak/facecheck/requestsResponses/workSite/data/SetNewCustomRadiusForWorkerInSpecialWorkSiteResponse.java
+++ face-check/SecurityDomainModule/src/main/java/com/zikpak/facecheck/requestsResponses/workSite/data/SetNewCustomRadiusForWorkerInSpecialWorkSiteResponse.java
@@ -1,10 +1,14 @@
 package com.zikpak.facecheck.requestsResponses.workSite.data;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class SetNewCustomRadiusForWorkerInSpecialWorkSiteResponse {
 
 
