41ec4783090468af2a91efd5da93500dffbd46b1
Mishar025 <mishamay583@gmail.com>
2025-07-27 22:14:05 -0400

    ,

--- face-check/SecurityDomainModule/src/main/java/com/zikpak/facecheck/requestsResponses/admin/ChangePunchOutRequest.java
+++ face-check/SecurityDomainModule/src/main/java/com/zikpak/facecheck/requestsResponses/admin/ChangePunchOutRequest.java
@@ -1,6 +1,10 @@
 package com.zikpak.facecheck.requestsResponses.admin;
 
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,14 +14,19 @@ import java.time.LocalTime;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ChangePunchOutRequest {
 
-
+    @NotNull(message = "This field is required!")
     private Integer workerId;
 
+    @NotNull(message = "This field is required!")
     private LocalDateTime dateWhenWorkerDidntMakePunchOut;
 
+    @NotNull(message = "This field is required!")
     private LocalDate newPunchOutDate;
 
+    @NotNull(message = "This field is required!")
     private LocalTime newPunchOutTime;
 }
