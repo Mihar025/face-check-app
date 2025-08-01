4bc950d8e375c51f69f02b60406f01663a21983f
Mishar025 <mishamay583@gmail.com>
2025-07-31 22:19:05 -0400

    ,

--- face-check/SecurityDomainModule/src/main/java/com/zikpak/facecheck/requestsResponses/finance/PayStubResponse.java
+++ face-check/SecurityDomainModule/src/main/java/com/zikpak/facecheck/requestsResponses/finance/PayStubResponse.java
@@ -1,12 +1,16 @@
 package com.zikpak.facecheck.requestsResponses.finance;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PayStubResponse {
 
     private BigDecimal grossPay;
