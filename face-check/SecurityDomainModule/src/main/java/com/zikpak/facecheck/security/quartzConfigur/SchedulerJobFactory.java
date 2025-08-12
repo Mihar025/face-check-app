c78698d0392f00f3423519e35a3eaec7a9cdcf7b
Mishar025 <mishamay583@gmail.com>
2025-08-11 21:16:43 -0400

    ,

---
+++ face-check/SecurityDomainModule/src/main/java/com/zikpak/facecheck/security/quartzConfigur/SchedulerJobFactory.java
@@ -0,0 +1,26 @@
+package com.zikpak.facecheck.security.quartzConfigur;
+
+import lombok.RequiredArgsConstructor;
+import org.quartz.spi.TriggerFiredBundle;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.scheduling.quartz.SpringBeanJobFactory;
+
+@RequiredArgsConstructor
+public class SchedulerJobFactory extends SpringBeanJobFactory implements ApplicationContextAware {
+
+    private  AutowireCapableBeanFactory beanFactory;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        beanFactory = applicationContext.getAutowireCapableBeanFactory();
+    }
+
+    @Override
+    protected Object createJobInstance(TriggerFiredBundle bundle) throws Exception {
+        Object job = super.createJobInstance(bundle);
+        beanFactory.autowireBean(job);
+        return job;
+    }
+}
