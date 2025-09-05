ed21d50438481af0b17356da131a75132d137fc2
Mishar025 <mishamay583@gmail.com>
2025-09-05 17:44:51 -0400

    ,

--- face-check/SecurityDomainModule/src/main/java/com/zikpak/facecheck/domain/CompanyServiceImpl.java
+++ face-check/SecurityDomainModule/src/main/java/com/zikpak/facecheck/domain/CompanyServiceImpl.java
@@ -9,10 +9,7 @@ public interface CompanyServiceImpl {
     String companyPhone(Authentication authentication);
     String companyEmail(Authentication authentication);
 
-    void updateCompanyName(String name, Authentication authentication);
-    void updateCompanyAddress(String name, Authentication authentication);
-    void updateCompanyPhoneNumber(String name, Authentication authentication);
-    void updateCompanyEmail(String name, Authentication authentication);
+
 
 
 }
