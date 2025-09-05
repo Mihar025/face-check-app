27af5c8cf6dda59ebe0bfebcfeee963e0a69d343
Mishar025 <mishamay583@gmail.com>
2025-09-05 17:45:12 -0400

    ,

--- face-check-ui/src/app/services/fn/company-controller/get-company-email.ts
+++ face-check-ui/src/app/services/fn/company-controller/get-company-email.ts
@@ -18,7 +18,7 @@ export function getCompanyEmail(http: HttpClient, rootUrl: string, params?: GetC
   }
 
   return http.request(
-    rb.build({ responseType: 'json', accept: 'application/json', context })
+    rb.build({ responseType: 'text', accept: 'text/plain', context })
   ).pipe(
     filter((r: any): r is HttpResponse<any> => r instanceof HttpResponse),
     map((r: HttpResponse<any>) => {
