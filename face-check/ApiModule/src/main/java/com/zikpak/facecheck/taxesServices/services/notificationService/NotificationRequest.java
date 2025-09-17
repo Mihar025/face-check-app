8827dac1a3e4530f251e626867604f1940380877
Mishar025 <mishamay583@gmail.com>
2025-09-16 22:58:01 -0400

    ,

---
+++ face-check/ApiModule/src/main/java/com/zikpak/facecheck/taxesServices/services/notificationService/NotificationRequest.java
@@ -0,0 +1,15 @@
+package com.zikpak.facecheck.taxesServices.services.notificationService;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class NotificationRequest {
+
+    private String message;
+
+}
