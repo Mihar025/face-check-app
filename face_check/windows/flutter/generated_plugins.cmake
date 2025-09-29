055b8ea974011056e55e644bb61e448206f7b102
Mishar025 <mishamay583@gmail.com>
2025-09-29 16:21:39 -0400

    .

--- face_check/windows/flutter/generated_plugins.cmake
+++ face_check/windows/flutter/generated_plugins.cmake
@@ -3,10 +3,12 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  battery_plus
   file_selector_windows
   flutter_secure_storage_windows
   geolocator_windows
   local_auth_windows
+  permission_handler_windows
   printing
   share_plus
   url_launcher_windows
