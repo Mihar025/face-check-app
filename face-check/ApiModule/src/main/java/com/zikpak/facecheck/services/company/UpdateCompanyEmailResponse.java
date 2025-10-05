caefb0a82530e8cda7dbd9c075abe6ab3a894148
Mishar025 <mishamay583@gmail.com>
2025-10-05 17:31:30 -0400

    .

---
+++ face-check/ApiModule/src/main/java/com/zikpak/facecheck/services/company/UpdateCompanyEmailResponse.java
@@ -0,0 +1,12 @@
+package com.zikpak.facecheck.services.company;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UpdateCompanyEmailResponse {
+
+        private String email;
+
+}
