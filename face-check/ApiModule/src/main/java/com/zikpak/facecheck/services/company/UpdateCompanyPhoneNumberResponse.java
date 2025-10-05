4da677542e3150afc93373f272a23a5d72b9d2d3
Mishar025 <mishamay583@gmail.com>
2025-10-05 17:31:38 -0400

    .

---
+++ face-check/ApiModule/src/main/java/com/zikpak/facecheck/services/company/UpdateCompanyPhoneNumberResponse.java
@@ -0,0 +1,14 @@
+package com.zikpak.facecheck.services.company;
+
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UpdateCompanyPhoneNumberResponse {
+
+private String phoneNumber;
+
+
+}
