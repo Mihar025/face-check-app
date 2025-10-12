eca82de3354ea023d0a46c78ab8d52d67b74aa90
Mishar025 <mishamay583@gmail.com>
2025-10-11 23:32:10 -0400

    .

--- face-check-ui/src/app/services/fn/company-controller/update-company-phone.ts
+++ face-check-ui/src/app/services/fn/company-controller/update-company-phone.ts
@@ -8,23 +8,25 @@ import { filter, map } from 'rxjs/operators';
 import { StrictHttpResponse } from '../../strict-http-response';
 import { RequestBuilder } from '../../request-builder';
 
+import { UpdateCompanyPhoneNumberRequest } from '../../models/update-company-phone-number-request';
+import { UpdateCompanyPhoneNumberResponse } from '../../models/update-company-phone-number-response';
 
 export interface UpdateCompanyPhone$Params {
-      body: string
+      body: UpdateCompanyPhoneNumberRequest
 }
 
-export function updateCompanyPhone(http: HttpClient, rootUrl: string, params: UpdateCompanyPhone$Params, context?: HttpContext): Observable<StrictHttpResponse<void>> {
+export function updateCompanyPhone(http: HttpClient, rootUrl: string, params: UpdateCompanyPhone$Params, context?: HttpContext): Observable<StrictHttpResponse<UpdateCompanyPhoneNumberResponse>> {
   const rb = new RequestBuilder(rootUrl, updateCompanyPhone.PATH, 'put');
   if (params) {
     rb.body(params.body, 'application/json');
   }
 
   return http.request(
-    rb.build({ responseType: 'text', accept: '*/*', context })
+    rb.build({ responseType: 'json', accept: 'application/json', context })
   ).pipe(
     filter((r: any): r is HttpResponse<any> => r instanceof HttpResponse),
     map((r: HttpResponse<any>) => {
-      return (r as HttpResponse<any>).clone({ body: undefined }) as StrictHttpResponse<void>;
+      return r as StrictHttpResponse<UpdateCompanyPhoneNumberResponse>;
     })
   );
 }
