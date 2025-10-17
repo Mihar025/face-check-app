334e38db1b8f4e0241369fbeea53ec23401b1065
Mishar025 <mishamay583@gmail.com>
2025-10-17 19:08:48 -0400

    .

--- face-check/AppStarterModule/src/main/resources/db/migration/V20__alter_table_tOu.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE terms_of_use_agreement RENAME COLUMN userid        TO user_id;
-ALTER TABLE terms_of_use_agreement RENAME COLUMN "timestamp"   TO time_stamp;
-ALTER TABLE terms_of_use_agreement RENAME COLUMN termsversion  TO terms_version;
-ALTER TABLE terms_of_use_agreement RENAME COLUMN privacyversion TO privacy_version;
-ALTER TABLE terms_of_use_agreement RENAME COLUMN osversion     TO os_version;
