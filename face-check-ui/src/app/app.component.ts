e2da354421e481112ad477f5402cd80e91b9dce6
Mishar025 <mishamay583@gmail.com>
2025-10-25 15:44:52 -0400

    .

--- face-check-ui/src/app/app.component.ts
+++ face-check-ui/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "./modules/main-page/additionalServices/auth-service";
+import {UserDataService} from "./modules/components/user-data-service/user-data-service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'face-check-ui';
+
+  constructor(
+    private authService: AuthService,
+    private userDataService: UserDataService
+  ) {}
+
+  ngOnInit() {
+    if (this.authService.isUserAuthenticated()) {
+      console.log('User authenticated, loading data...');
+      this.userDataService.loadUserData();
+    }
+  }
+
+
 }
