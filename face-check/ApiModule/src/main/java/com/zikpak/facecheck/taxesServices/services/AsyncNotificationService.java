9b43324fe5bb4f6764afe10034c1f2b714cacd3d
Mishar025 <mishamay583@gmail.com>
2026-01-25 22:44:11 -0500

    .

--- face-check/ApiModule/src/main/java/com/zikpak/facecheck/taxesServices/services/AsyncNotificationService.java
+++ face-check/ApiModule/src/main/java/com/zikpak/facecheck/taxesServices/services/AsyncNotificationService.java
@@ -34,6 +34,7 @@ public class AsyncNotificationService {
                             today + " in " +
                             worksiteName + " " +
                             workSiteAddress)
+                    .adminOnly(true)
                     .build();
             notificationService.createNotification(companyId, request);
         }
@@ -47,6 +48,7 @@ public class AsyncNotificationService {
                             today + " in " +
                             worksiteName + " " +
                             workSiteAddress)
+                    .adminOnly(true)
                     .build();
             notificationService.createNotification(companyId, request);
         }
