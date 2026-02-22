5c039c1d9f006a1745d20e926c2a743f315faf9c
Mishar025 <mishamay583@gmail.com>
2026-02-22 16:28:07 -0500

    .

--- face-check/SecurityDomainModule/src/main/java/com/zikpak/facecheck/repository/LocationRecordRepository.java
+++ face-check/SecurityDomainModule/src/main/java/com/zikpak/facecheck/repository/LocationRecordRepository.java
@@ -15,18 +15,16 @@ import java.util.Optional;
 @Repository
 public interface LocationRecordRepository extends JpaRepository<LocationRecord, Integer> {
 
-
     @Query(value = """
     SELECT lr.* FROM location_record lr
     WHERE lr.user_id = :userId
-    AND DATE(lr.timestamp AT TIME ZONE 'UTC' AT TIME ZONE 'America/New_York') = :date
-    ORDER BY lr.timestamp ASC
+    AND DATE(lr."timestamp" AT TIME ZONE 'UTC' AT TIME ZONE 'America/New_York') = :date
+    ORDER BY lr."timestamp" ASC
     """, nativeQuery = true)
     List<LocationRecord> findByUserAndDate(
             @Param("userId") Integer userId,
             @Param("date") LocalDate date
     );
-
 
     @Query("""
     SELECT lr FROM LocationRecord lr
